ba519b3a3aacbe35563e1ebb48d25a3c00dcbed7
MCrutch <melanie_crutchfield@hotmail.com>
2016-01-09 16:03:33 -0800

    Test email as management command.

--- courier/management/commands/send_heroku_test_email.py
+++ courier/management/commands/send_heroku_test_email.py
@@ -1,20 +1,29 @@
+from django.core.management.base import BaseCommand
 import email
 import smtplib
 
-msg_to = "6102032488@vmobl.com"
-msg = email.message_from_string("Ooh! The test worked!")
-msg['From'] = "melanie_crutchfield@hotmail.com"
-msg['To'] = msg_to
-msg['Subject'] = "" #can leave blank
+def testemail():
+    print("I'm going to make an email.")
+    msg_to = "6102032488@vmobl.com"
+    msg = email.message_from_string("Ooh! The test worked!")
+    msg['From'] = "melanie_crutchfield@hotmail.com"
+    msg['To'] = msg_to
+    msg['Subject'] = "" #can leave blank
 
-s = smtplib.SMTP("smtp.live.com",587)
-s.ehlo()
-s.starttls() 
-s.ehlo()
-s.login('melanie_crutchfield@hotmail.com', 'crutchback') # TODO set up a user with a password that correlates
-#to sending email account
+    print("Here comes the sending!")
+    s = smtplib.SMTP("smtp.live.com",587)
+    s.ehlo()
+    s.starttls() 
+    s.ehlo()
+    s.login('melanie_crutchfield@hotmail.com', 'crutchback') # TODO set up a user with a password that correlates
+    print("I'm logged in")
+    #to sending email account
 
-# s.sendmail(sender, recipient phone number, message) 6192032488@vmobl.com
-s.sendmail("melanie_crutchfield@hotmail.com", msg_to, msg.as_string())
+    # s.sendmail(sender, recipient phone number, message) 6192032488@vmobl.com
+    s.sendmail("melanie_crutchfield@hotmail.com", msg_to, msg.as_string())
 
-s.quit()
+    s.quit()
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        testemail()
