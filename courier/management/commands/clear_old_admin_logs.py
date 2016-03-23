5bc588bd4a5f2f87d0c6e2635e95e0fca7101171
MCrutch <melanie_crutchfield@hotmail.com>
2016-03-22 18:47:40 -0700

    added management commands to clear old records to limit database row usage.

--- courier/management/commands/clear_old_admin_logs.py
+++ courier/management/commands/clear_old_admin_logs.py
@@ -1,6 +1,13 @@
+from django.core.management.base import BaseCommand
 from django.contrib.admin.models import LogEntry
 
-logs = LogEntry.objects.all()
 
-for i in range(2000,len(logs)):
-	logs[i].delete()
+def clear_old_admin_logs():
+	logs = LogEntry.objects.all()
+
+	for i in range(2000,len(logs)):
+		logs[i].delete()
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        clear_old_admin_logs()
