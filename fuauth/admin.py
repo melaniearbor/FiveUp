19f177a28753d167e2625127b2c0d79be605b4a2
HelloMelanieC <hellomelaniec@gmail.com>
2017-05-21 14:41:25 -0700

    Made a search field in the admin and changed the link sharing area to be clearer.

--- fuauth/admin.py
+++ fuauth/admin.py
@@ -10,54 +10,56 @@ from courier.models import UserSendTime
 
 class CustomUserAdmin(admin.ModelAdmin):
 
-	add_form = FUserCreationForm
+    add_form = FUserCreationForm
 
-	readonly_fields = ('date_joined', 'uuid')
+    readonly_fields = ('date_joined', 'uuid')
 
-	fieldsets = (
-		(None, {'fields': ('email', 'password', 'receive_newsletter')}),
-		('Personal info', {'fields': ('name', 'phone_number', 'carrier', 'user_timezone')}),
-		('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'receiving_messages', 'how_many_messages')}),
-		('Auto info', {'fields': ('date_joined', 'uuid')}),
-	)
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'receive_newsletter')}),
+        ('Personal info', {'fields': ('name', 'phone_number', 'carrier', 'user_timezone')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'receiving_messages', 'how_many_messages')}),
+        ('Auto info', {'fields': ('date_joined', 'uuid')}),
+    )
 
-	list_display = ('email', 'name', 'phone_number', 'is_active', 'is_staff',
-		'receive_newsletter', 'receiving_messages', 'how_many_messages', 'get_carrier_display')
-	list_editable = ('is_active', 'receive_newsletter', 'receiving_messages')
-	list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups',
-		'receive_newsletter', 'receiving_messages', 'carrier')
-	search_fields = ('email', 'name', 'phone_number', 'carrier')
-	ordering = ('email', 'name')
-	column = ('total_user_count',)
+    list_display = ('email', 'name', 'phone_number', 'is_active', 'is_staff',
+        'receive_newsletter', 'receiving_messages', 'how_many_messages', 'get_carrier_display')
+    list_editable = ('is_active', 'receive_newsletter', 'receiving_messages')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups',
+        'receive_newsletter', 'receiving_messages', 'carrier')
+    search_fields = ('email', 'name', 'phone_number', 'carrier')
+    ordering = ('email', 'name')
+    column = ('total_user_count',)
 
-	def total_user_count(self, obj):
-		return User.objects.count()
+    def total_user_count(self, obj):
+        return User.objects.count()
 
 class CuratedMessageAdmin(admin.ModelAdmin):
 
     add_form = CuratedMessageForm
 
     fieldsets = (
-    	('Banana Town', {'fields': ('message_text', 'message_author_first', 'message_author_last')}),
-	)
+        ('Banana Town', {'fields': ('message_text', 'message_author_first', 'message_author_last')}),
+    )
+
+    search_fields = ('message_author_last',)
 
 class UserSendTimeAdmin(admin.ModelAdmin):
 
-	list_display = ('user_name', 'user', 'scheduled_time', 'sent')
+    list_display = ('user_name', 'user', 'scheduled_time', 'sent')
 
-	search_fields = ('user__name', 'user__email', 'user__phone_number')
+    search_fields = ('user__name', 'user__email', 'user__phone_number')
 
-	ordering = ('user', 'scheduled_time', 'sent')
+    ordering = ('user', 'scheduled_time', 'sent')
 
-	list_filter = ('sent',)
+    list_filter = ('sent',)
 
-	date_hierarchy = 'scheduled_time'
+    date_hierarchy = 'scheduled_time'
 
-	def user_name(self, obj):
-	    return obj.user.name
+    def user_name(self, obj):
+        return obj.user.name
 
-	def user_email(self, obj):
-	    return obj.user.email
+    def user_email(self, obj):
+        return obj.user.email
 
 
 
