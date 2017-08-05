3a85689604c095bda2db1b6cf0536c50659d113c
HelloMelanieC <hellomelaniec@gmail.com>
2017-08-04 21:03:05 -0700

    Added file for ssl cert and view to serve.

--- fiveup/urls.py
+++ fiveup/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 import messagebox.views
 import fuauth.views
 import fuauth.forms
+from .views import display_cert
 
 urlpatterns = patterns('',
     # Examples:
@@ -17,20 +18,21 @@ urlpatterns = patterns('',
     # a way to store for a certain user in the messages db
     url(r'^new/(?P<uuid>.+)/$', messagebox.views.CreateMessageView.as_view(),
     name='add-message-view',),
-    url(r'^add-message-success/', messagebox.views.success, 
+    url(r'^add-message-success/', messagebox.views.success,
     	name='add-message-success',),
     url(r'^signup/', fuauth.forms.PublicUserCreation.as_view(),
         name='signup-form',),
     url(r'^register/', fuauth.views.register,
         name='register',),
-    url(r'^loginuser/', fuauth.views.login_user, 
+    url(r'^loginuser/', fuauth.views.login_user,
         name='loginuser',),
-    url(r'^logoutuser/', fuauth.views.logout_user, 
+    url(r'^logoutuser/', fuauth.views.logout_user,
         name='logoutuser',),
-    url(r'^changeprofile/(?P<uuid>.+)/$', fuauth.forms.FiveUUserChangeForm.as_view(), 
+    url(r'^changeprofile/(?P<uuid>.+)/$', fuauth.forms.FiveUUserChangeForm.as_view(),
         name='changeprofile',),
-    url(r'^contact/', messagebox.views.contact, 
+    url(r'^contact/', messagebox.views.contact,
         name='contact',),
+    url(r'^.well-known/pki-validation/BA7CD11C05866445FBFE053E2C1AAA8C.txt', display_cert),
 )
 
 STATIC_URL = '/static/'
---
+++ fiveup/views.py
@@ -0,0 +1,11 @@
+import os
+from django.conf import settings
+from django.shortcuts import render, render_to_response, redirect
+from django.http import HttpRequest, HttpResponse
+
+def display_cert(request):
+    file_path = os.path.join(settings.STATIC_ROOT, 'BA7CD11C05866445FBFE053E2C1AAA8C.txt')
+    with open(file_path, 'rb') as txt_file:
+        response = HttpResponse(txt_file.read(), content_type="text/plain")
+        response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+        return response
