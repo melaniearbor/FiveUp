94fa11b1ca954e33a393fcbc5ecbf5856b725d0d
HelloMelanieC <hellomelaniec@gmail.com>
2017-08-08 09:25:06 -0700

    Add turning django-sslify off for local production. Move ssl-ify to first middleware.

--- settings/local.py
+++ settings/local.py
@@ -3,3 +3,5 @@ from .base import *
 DEBUG = True
 
 TEMPLATE_DEBUG = True
+
+SSLIFY_DISABLE = True
--- settings/production.py
+++ settings/production.py
@@ -5,6 +5,7 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 
 MIDDLEWARE_CLASSES = (
+    'sslify.middleware.SSLifyMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -12,5 +13,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'sslify.middleware.SSLifyMiddleware',
 )
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
